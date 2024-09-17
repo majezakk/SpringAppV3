9bec35a0c21eae54e494702bd5a629c424f702a3
zakk_pc <zakhar.dm.ivanov@gmail.com>
2024-09-17 19:34:21 +0300

    Реализована главная страницы веб-приложения

--- src/main/java/com/example/SpringAppV3/SpringAppV3Application.java
+++ src/main/java/com/example/SpringAppV3/SpringAppV3Application.java
@@ -2,6 +2,9 @@ package com.example.SpringAppV3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringAppV3Application {
@@ -10,4 +13,13 @@ public class SpringAppV3Application {
 		SpringApplication.run(SpringAppV3Application.class, args);
 	}
 
+	//Конфигурация для обработки статических ресурсов
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer {
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry.addResourceHandler("/static/**")
+					.addResourceLocations("classpath:/static/");
+		}
+	}
 }
