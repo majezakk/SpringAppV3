bdf3089dde04da8f4b67e203e1a9fc8d697924b0
zakk_pc <zakhar.dm.ivanov@gmail.com>
2024-09-17 15:38:37 +0300

    Initial commit

---
+++ src/main/java/com/example/SpringAppV3/SpringAppV3Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringAppV3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringAppV3/SpringAppV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringAppV3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
